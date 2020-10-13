c64a10a844dd1f00eee4dd51642d66a3d8f1a6e0
Rick Vickery <rickvickery@quickenloans.com>
2020-10-12 23:36:30 -0400

    These are the project files for AngularTwo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,17 +12,17 @@ export class AppComponent {
 
  posts: Post[] = [
    {title:'Its a great day for hay.',thought:'I had a lot of mac and cheese.'},
-   {title:'Thats barley.',thought:'But seriously, Oh lord, my belly.'},
-   {title:'What is life?',thought:'Too long sometimes.'},
+   {title:'Thats barley.',thought:'But seriously, what?'},
+   {title:'What is life?',thought:'IDEK.'},
    {title:'What is the universe?',thought:'Aint nobody got time for dat'},
-   {title:'THIS IS JEOPARDY',thought:'Daily Double.'}
+   {title:'THIS IS JEOPARDY',thought:'Daily Double..?'}
  ];
 
 postDelete = function($event) {
     let x: number = 0;
 
     for(let post of this.posts) {
-      if(post.thought == $event.thought && post.title == $event.title){
+      if(post.title == $event.title){
         this.posts.splice(x, 1);
       }
       x++;
